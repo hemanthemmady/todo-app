2ce4183aecb9f7fb6f21468a591ce370d883683f
Hemant Hemmady <hemanthemmady@gmail.com>
2021-02-20 18:28:17 +0530

    Add styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import TodoItem from './components/TodoItem';
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(['Todo 1']);
   const addTodoHandler = event => {
     event.preventDefault();
-    setTodos([...todos, event.target.elements.todoInput.value]);
+    const todo = event.target.elements.todoInput.value;
+    todo && setTodos([...todos, todo]);
+    console.log(event.target.elements);
     event.target.elements.todoInput.value = '';
   };
   const deleteTodoHandler = todoToDelete => {
@@ -16,14 +18,25 @@ function App() {
     <TodoItem key={index} value={todo} delete={() => deleteTodoHandler(todo)} />
   ));
   return (
-    <div className="container">
-      <header>
+    <div className="mx-auto max-w-md mt-8 h-96 rounded shadow-lg flex flex-col">
+      <header className="flex justify-center content-center pt-3 pb-5">
         <h1 className="text-2xl">Todo App</h1>
       </header>
-      <div>{todosList}</div>
-      <form onSubmit={addTodoHandler}>
-        <input id="todoInput" type="text" />
-        <button type="submit">Add Task</button>
+      <div className="flex-grow max-h-screen">{todosList}</div>
+      <form className="flex" onSubmit={addTodoHandler}>
+        <input
+          id="todoInput"
+          type="text"
+          className="flex-grow m-1 rounded border-2 border-gray-700 focus:outline-none focus:ring focus:border-green-700"
+          placeholder="Add task..."
+        />
+        <button
+          type="submit"
+          id="todoSubmit"
+          className="p-2 m-1 rounded shadow bg-gray-700 text-white hover:bg-green-700"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,9 +2,27 @@ import React from 'react';
 
 const TodoItem = props => {
   return (
-    <div>
-      <p>{props.value}</p>
-      <button onClick={props.delete}>x</button>
+    <div className="flex p-2 mr-1 ml-1 border-b-2">
+      <p className="flex-grow truncate">{props.value}</p>
+      <button
+        className="flex items-center justify-center text-gray-700 hover:text-red-700 focus:outline-none"
+        onClick={props.delete}
+      >
+        <svg
+          className="w-7 h-7"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      </button>
     </div>
   );
 };
